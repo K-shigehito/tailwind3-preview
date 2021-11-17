e53caf5349ec59a945df2fe38122bfcd141bfb08
K-shigehito <example@test.com>
2021-11-17 12:50:47 +0900

    fix: aspect-ratio 比率の追加

--- src/components/AspectRatio.vue
+++ src/components/AspectRatio.vue
@@ -1,15 +1,40 @@
 <template>
-  <div
-    class="
-      flex
-      items-center
-      justify-center
-      w-[300px]
-      aspect-[16/9]
-      bg-indigo-200
-      text-100
-    "
-  >
-    aspect-ratio: 16/9
+  <div class="flex gap-[20px] items-start">
+    <div
+      class="
+        flex flex-grow
+        items-center
+        justify-center
+        aspect-[2/3]
+        bg-indigo-200
+        text-100
+      "
+    >
+      2/3
+    </div>
+    <div
+      class="
+        flex flex-grow
+        items-center
+        justify-center
+        aspect-square
+        bg-amber-200
+        text-100
+      "
+    >
+      square
+    </div>
+    <div
+      class="
+        flex flex-grow
+        items-center
+        justify-center
+        aspect-[16/9]
+        bg-rose-200
+        text-100
+      "
+    >
+      16/9
+    </div>
   </div>
 </template>
